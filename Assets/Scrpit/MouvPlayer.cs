8403afbc578a24735a7cecd2f7c2838b8c40a530
quentin rabot <77061987+rabot-quentin@users.noreply.github.com>
2023-04-29 15:59:16 +0200

    premier push

---
+++ Assets/Scrpit/MouvPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvPlayer : MonoBehaviour
+{
+    public int speed;
+    public int jumpForce;
+   // public float gravity ;
+   
+
+    public CharacterController controller; 
+
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump"); 
+
+        Vector3 direction = new Vector3(horizontal, 0f , vertical).normalized;
+        Vector3 Vecjump = new Vector3(0f, jump, 0f).normalized;
+
+        direction.y -= gravity * Time.deltaTime;
+
+       
+            if ((direction.magnitude >= 0.1f)) // deplacement joueur 
+            {
+                float targetAngle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+                controller.Move(direction * Time.deltaTime * speed);
+            }
+        
+       //direction.y -= gravity * Time.deltaTime;
+        if (controller.isGrounded) // saut joueur 
+        {            
+           controller.Move(Vecjump * Time.deltaTime * jumpForce); 
+        }
+        Vector3 graviter = new Vector3(0, -9.81f, 0);
+        flot graviterMultiplier = 10f; 
+       
+
+    }
+   
+}
